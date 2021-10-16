3120cf3c960cf96e52a88400720e159bf142da55
Vladomira <juliavladomira@gmail.com>
2021-10-16 12:02:34 +0300

    control

--- src/components/ContactList/ContactItem.js
+++ src/components/ContactList/ContactItem.js
@@ -5,8 +5,7 @@ const { v4: uuidv4 } = require("uuid");
 const ContactItem = ({ name, number, id, deleteContact }) => {
   return (
     <li id={uuidv4()} key={id} className="contacts__item">
-      {name}
-      <span>: {number}</span>
+      {name} <span>: {number}</span>
       <button className="contacts__btn" onClick={() => deleteContact(id)}>
         Delete
       </button>
